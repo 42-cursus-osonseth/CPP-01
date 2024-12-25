b56e42ab5af08a363525c1851c92e3b8a869740c
Osonseth <max.cesaria@gmail.com>
2024-12-25 15:18:40 +0100

    Ex01 pret pour evaluation

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -1,15 +1,13 @@
 #include "Zombie.hpp"
 
+Zombie::Zombie(){}
 
 Zombie::Zombie(std::string name)
 {
     this->name = name;
     announce();
 }
-Zombie::Zombie()
-{
 
-}
 Zombie::~Zombie()
 {
     std::cout << name << ": IS DEAD !" << std::endl;
@@ -18,4 +16,10 @@ Zombie::~Zombie()
 void Zombie::announce(void) const
 {
     std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie* Zombie::newZombie( std::string name )
+{
+    Zombie *z = new Zombie(name);
+    return z;
 }
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -2,8 +2,7 @@
 
 Zombie *Zombie::zombieHorde(int N, std::string name)
 {
-    Zombie *z = new Zombie[N];
+    Zombie *z = new Zombie[N]; 
     for (int i = 0; i < N; i++)
-        new (&z[i]) Zombie(name);
-    return z;
+        new (&z[i]) Zombie(name); 
 }
